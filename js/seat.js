93888936a45ae0fd26bb395c49bf265ce87e7b3c
emilywin825 <102593109+emilywin825@users.noreply.github.com>
2023-03-28 18:45:55 +0900

    js 시작

--- js/seat.js
+++ js/seat.js
@@ -1,14 +1,27 @@
 
+//페이지 새로고침
+// onClick="window.location.reload()
 
 
+
+
+
+
+
+// 인원 선택 버튼
+//querySelectorAll로 받아오기
 const $minus_button=document.querySelector('.minus_button');
 const $plus_button=document.querySelector('.plus_button');
-const $selected_seat=document.querySelector('.selected_seat');
+const $selectd_seat=document.querySelector('.selectd_seat');
 
-$plus_button.onclick=function(){
-    $selected_seat.innerHTML='1' ;
-}
+$plus_button.onclick = function(){
+    $selectd_seat.innerHTML++;
+};
 
-
+$minus_button.onclick = function(){
+    if($selectd_seat.innerHTML!=0)
+     $selectd_seat.innerHTML--;
+};
+// 인원 선택 버튼
 
 
