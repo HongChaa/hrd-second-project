b6a46b9dc2b2712b1f5c9cfe32d24a2084bddc1e
emilywin825 <102593109+emilywin825@users.noreply.github.com>
2023-03-28 18:17:39 +0900

    수정중

--- js/seat.js
+++ js/seat.js
@@ -1,11 +1,12 @@
 
 
+
 const $minus_button=document.querySelector('.minus_button');
 const $plus_button=document.querySelector('.plus_button');
 const $selected_seat=document.querySelector('.selected_seat');
 
 $plus_button.onclick=function(){
-    $selected_seat.innerHTML='1';
+    $selected_seat.innerHTML='1' ;
 }
 
 
