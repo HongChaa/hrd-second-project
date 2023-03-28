8006223b1ebe22bd7c980a70e7806d6ce1a24a33
emilywin825 <102593109+emilywin825@users.noreply.github.com>
2023-03-28 17:11:28 +0900

    html/css 하는 중

---
+++ js/seat.js
@@ -0,0 +1,13 @@
+
+
+const $minus_button=document.querySelector('.minus_button');
+const $plus_button=document.querySelector('.plus_button');
+const $selected_seat=document.querySelector('.selected_seat');
+
+$plus_button.onclick=function(){
+    $selected_seat.innerHTML='1';
+}
+
+
+
+
