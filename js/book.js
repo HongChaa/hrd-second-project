a5a36ab7c5849892e70d02be9c1f8d2aa2365b57
HongChaa <126743377+HongChaa@users.noreply.github.com>
2023-03-30 21:56:06 +0900

    checkcontent JS시작

--- js/book.js
+++ js/book.js
@@ -1,26 +1,25 @@
 // <영화> 영화 목록 색깔 ======================================= start
 const $movielist = document.querySelector('.movie-list ul li');
 $movielist.parentElement.onclick = e => {
-   const $selected = e.target.parentElement.querySelector('.change-color-movie');
-
+    const $selected = e.target.parentElement.querySelector('.change-color-movie');
+    const $movielistSoldout = e.target.querySelector('.movie-list ul .soldout');
+    
     if($selected !== null) $selected.classList.remove('change-color-movie');
     
     e.target.classList.add('change-color-movie');
+    bookInfo.title = e.target.textContent;
 
-    // if($selected.classList.contains('.soldout')) alert(`해당 영화는 매진입니다.`);
+    document.querySelector('.seletedTitle').textContent = bookInfo.title;
+
+    if(e.target.classList.contains('soldout')) {
+        alert(`해당 영화는 매진입니다.`); 
+        e.target.classList.remove('change-color-movie');
+    }
 };
 
 // <영화> 영화 목록 색깔 ======================================= end
 
-
-// <영화> 매진된 영화 알람 ================================== start
-
-
-
-// <영화> 매진된 영화 알람 ================================== start
-
-
-
+  
 // <극장> 지역 목록 색깔 $ display ======================================= start
 
 const $region = document.querySelector('.region');
@@ -58,7 +57,7 @@ $detailCity.parentElement.onclick = e => {
     if ($city !== null) $city.classList.remove('change-color-city');
 
          e.target.classList.add('change-color-city');
-
+        bookInfo.location =  e.target.textContent;
 }
 
 // <극장> detail지역 목록 색깔 $ display ======================================= end
@@ -72,9 +71,18 @@ const $daydate = document.querySelector('.daydate');
 $daydate.parentElement.onclick = e => {
     
     const $selectedDatdate = e.target.parentElement.querySelector('div.change-color-daydate');
+    
+    const $year = e.target.parentElement.parentElement.querySelector('.year').textContent;
+    const $month = e.target.parentElement.parentElement.querySelector('.month').textContent;
+    const $detaildate = e.target.querySelector('.detaildate').textContent;
+    const $day = e.target.querySelector('.day').textContent;
+
     if($selectedDatdate !== null) $selectedDatdate.classList.remove('change-color-daydate');
     
     e.target.classList.add('change-color-daydate');
+    bookInfo.date = `${$year}.${$month}.${$detaildate} (${$day})`; 
+
+
 };
 
 // <시간> 시간 선택 색깔 ==================================== start
@@ -91,8 +99,47 @@ $room.parentElement.onclick = e => {
     if($selectedTime !== null) $selectedTime.classList.remove('change-color-playtime');
     
     e.target.classList.add('change-color-playtime');
+    bookInfo.room = e.target.parentElement.parentElement.parentElement.parentElement.querySelector('.section-title .typ').textContent;
+    bookInfo.startTime = e.target.textContent;
 
+    document.querySelector('.selectedRoom').textContent = e.target.parentElement.parentElement.parentElement.parentElement.querySelector('.section-title b').textContent;
 }
 
 
 // <시간> 시간 선택 색깔 ==================================== end
+
+
+
+// check-content ========================================== start
+
+// 영화 예약 정보 담는 객체
+const bookInfo = { 
+    title: "",
+    location: "",
+    date: "",
+    startTime: "",
+    room: "",
+    seat_id: ""
+
+};
+
+
+// check-content / poster-title 부분 ====================== start
+
+
+
+
+
+
+
+
+
+// check-content / poster-title 부분 ====================== end
+
+
+
+
+
+
+
+// check-content ========================================== end
