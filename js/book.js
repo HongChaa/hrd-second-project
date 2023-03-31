0443a6105e75c230c7e752c14cd17e3a45e5c8a9
HongChaa <126743377+HongChaa@users.noreply.github.com>
2023-03-31 16:16:46 +0900

    객체보내기미완성-book

--- js/book.js
+++ js/book.js
@@ -3,23 +3,48 @@ const $movielist = document.querySelector('.movie-list ul li');
 $movielist.parentElement.onclick = e => {
     const $selected = e.target.parentElement.querySelector('.change-color-movie');
     const $movielistSoldout = e.target.querySelector('.movie-list ul .soldout');
-    
-    if($selected !== null) $selected.classList.remove('change-color-movie');
-    
+    const $poster = document.querySelector('.poster');
+
+    if ($selected !== null) $selected.classList.remove('change-color-movie');
+
     e.target.classList.add('change-color-movie');
     bookInfo.title = e.target.textContent;
 
+    let selectedPic ='';
+
+    switch (bookInfo.title) {
+        case "더퍼스트슬램덩크": selectedPic ='slamdunk';
+            break;
+        case "스즈메의문단속": selectedPic ='suzume';
+            break;
+        case "던전앤드래곤-도적들의 명예": selectedPic ='deongeon';
+            break;
+        case "웅남이": selectedPic ='woongnam';
+            break;
+        case "엘카미노": selectedPic ='elcamino';
+            break;
+        case "소울메이트": selectedPic ='soulmate';
+            break;
+        case "오토라는남자": selectedPic ='otto';
+            break;
+        case "여섯개의밤": selectedPic ='layover';
+            break;
+            default : selectedPic ='';
+    }
+    if($poster.classList.contains('ddd') !== null) $poster.classList.remove('ddd');
+    $poster.classList.add(selectedPic);
+    $poster.classList.add('ddd');
     document.querySelector('.seletedTitle').textContent = bookInfo.title;
 
-    if(e.target.classList.contains('soldout')) {
-        alert(`해당 영화는 매진입니다.`); 
+    if (e.target.classList.contains('soldout')) {
+        alert(`해당 영화는 매진입니다.`);
         e.target.classList.remove('change-color-movie');
     }
 };
 
 // <영화> 영화 목록 색깔 ======================================= end
 
-  
+
 // <극장> 지역 목록 색깔 $ display ======================================= start
 
 const $region = document.querySelector('.region');
@@ -56,8 +81,12 @@ $detailCity.parentElement.onclick = e => {
 
     if ($city !== null) $city.classList.remove('change-color-city');
 
-         e.target.classList.add('change-color-city');
-        bookInfo.location =  e.target.textContent;
+    e.target.classList.add('change-color-city');
+    bookInfo.location = e.target.parentElement.querySelector('li.change-color-city').textContent;
+
+    // check-content<selected-location>  ==================================== start
+    document.querySelector('.selected-show-location em').textContent = bookInfo.location;
+    // check-content<selected-location>  ==================================== end
 }
 
 // <극장> detail지역 목록 색깔 $ display ======================================= end
@@ -69,19 +98,23 @@ const $daydateWrap = document.querySelector('.daydate-wrap');
 const $daydate = document.querySelector('.daydate');
 
 $daydate.parentElement.onclick = e => {
-    
+
     const $selectedDatdate = e.target.parentElement.querySelector('div.change-color-daydate');
-    
+
     const $year = e.target.parentElement.parentElement.querySelector('.year').textContent;
     const $month = e.target.parentElement.parentElement.querySelector('.month').textContent;
     const $detaildate = e.target.querySelector('.detaildate').textContent;
     const $day = e.target.querySelector('.day').textContent;
 
-    if($selectedDatdate !== null) $selectedDatdate.classList.remove('change-color-daydate');
-    
-    e.target.classList.add('change-color-daydate');
-    bookInfo.date = `${$year}.${$month}.${$detaildate} (${$day})`; 
+    if ($selectedDatdate !== null) $selectedDatdate.classList.remove('change-color-daydate');
 
+    e.target.classList.add('change-color-daydate');
+    bookInfo.date = `${$year}.${$month}.${$detaildate} (${$day})`;
+
+
+    // checkcontent<selected-date>   ==================================== start
+    document.querySelector('.selected-date').textContent = bookInfo.date;
+    // checkcontent<selected-date>   ==================================== end
 
 };
 
@@ -93,16 +126,19 @@ const $timeList = document.querySelector('.time-list');
 
 $room.parentElement.onclick = e => {
     console.log('이벤트출력');
-    
-    const $selectedTime = $room.parentElement.querySelector('.change-color-playtime');
-    
-    if($selectedTime !== null) $selectedTime.classList.remove('change-color-playtime');
-    
-    e.target.classList.add('change-color-playtime');
-    bookInfo.room = e.target.parentElement.parentElement.parentElement.parentElement.querySelector('.section-title .typ').textContent;
-    bookInfo.startTime = e.target.textContent;
 
-    document.querySelector('.selectedRoom').textContent = e.target.parentElement.parentElement.parentElement.parentElement.querySelector('.section-title b').textContent;
+    const $selectedTime = $room.parentElement.querySelector('.change-color-playtime');
+
+    if ($selectedTime !== null) $selectedTime.classList.remove('change-color-playtime');
+
+    e.target.classList.add('change-color-playtime');
+    bookInfo.room = e.target.parentElement.parentElement.parentElement.querySelector('.typ').textContent;
+    bookInfo.startTime = e.target.parentElement.querySelector('.start-time').textContent;
+    // check-content / poster-title 부분 
+    document.querySelector('.selectedRoom').textContent = e.target.parentElement.parentElement.parentElement.querySelector('b').textContent;
+    // check-content / book-info 시간부분 
+    document.querySelector('.selected-startTime').textContent = bookInfo.startTime;
+
 }
 
 
@@ -113,31 +149,29 @@ $room.parentElement.onclick = e => {
 // check-content ========================================== start
 
 // 영화 예약 정보 담는 객체
-const bookInfo = { 
+const bookInfo = {
     title: "",
     location: "",
     date: "",
     startTime: "",
     room: "",
-    seat_id: ""
-
 };
 
+const $nextBtn = document.querySelector('.next-btn-wrap a');
 
-// check-content / poster-title 부분 ====================== start
+window.onload = e => {
+    infoFullFlag = 1
 
+    // for (let infokey of bookInfo) {
+    //     if (bookInfo[infokey] !== "") {
+    //         infoFullFlag = -1
+    //     }
+    // }
 
-
-
-
-
-
-
-
-// check-content / poster-title 부분 ====================== end
-
-
-
+    // if (infoFullFlag === 1) {
+    //     $nextBtn.classList.add('change-color-btn');
+    // }
+}
 
 
 
